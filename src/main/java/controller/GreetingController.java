f5030c8d99b30c26e1be0ed44d8c97f614cc8d0f
Marcelo <marceloalvessoft@gmail.com>
2018-07-16 20:35:42 -0300

    incluindo JPA

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,13 +1,15 @@
 package controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import model.Evento;
 import model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
+
 public class GreetingController {
 
     private static final String template = "Helldo, %s!";
@@ -18,5 +20,12 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+
+    @RequestMapping("/")
+    public Evento index() {
+        return  new Evento("Evento escrotoao", "Evento escroto descricao");
+    }
+
 
 }
