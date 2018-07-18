ca0e534daaae34a37b5de5e8adc41952e0d046d2
Jesus <jesusadadfilho@gmail.com>
2018-07-18 20:12:58 -0300

    adicionando colluns na classe cupom

--- src/main/java/model/Cupom.java
+++ src/main/java/model/Cupom.java
@@ -1,15 +1,30 @@
 package model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 public class Cupom {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "codigo")
     private String codigo;
+
+    @JoinColumn(name = "evento_id", referencedColumnName = "id_eveneto")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
     private Evento evento;
+
+    @Column(name = "desconto")
     private float desconto;
+
+    @Column(name = "dataValidade")
     private Date dataValidade;
 
     public Cupom(String descricao, Evento evento, float desconto) {
