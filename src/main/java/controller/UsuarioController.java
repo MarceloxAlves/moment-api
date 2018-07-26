2dd898a0306606eddeeb5bd81fafc406f9b70fb3
Marcelo <marceloalvessoft@gmail.com>
2018-07-26 02:25:30 -0300

    Corrigindo Rotas

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,10 +1,8 @@
 package controller;
 
-import model.Evento;
 import model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import repository.EventoRepository;
 import repository.UsuarioRepository;
 
 import javax.validation.Valid;
@@ -18,17 +16,17 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping("/usuario/listar")
+    @GetMapping("/listar")
     public List<Usuario> getAllEventos() {
         return usuarioRepository.findAll();
     }
 
-    @GetMapping("/usuario/count")
+    @GetMapping("/count")
     public long getTotal() {
         return usuarioRepository.count();
     }
 
-    @PostMapping(path = "usuario/cadastrar",  consumes = "application/json", produces = "application/json")
+    @PostMapping(path = "/cadastrar",  consumes = "application/json", produces = "application/json")
     public Usuario cadastrarUsuario(@Valid @RequestBody Usuario usuario) {
           return usuarioRepository.save(usuario);
     }
