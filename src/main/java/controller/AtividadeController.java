40357f4dd7de278a621f39f7551b24501415b349
Icarus09 <i.victorfs@gmail.com>
2018-08-12 01:49:59 -0300

    Criando recursos de Atividade

--- src/main/java/controller/AtividadeController.java
+++ src/main/java/controller/AtividadeController.java
@@ -3,6 +3,7 @@ package controller;
 import model.Atividade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import service.AtividadeService;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/atividade")
@@ -28,7 +30,14 @@ public class AtividadeController {
     }
 
     @GetMapping("/cadastrar")
-    public Atividade criarCupom(@Valid @RequestBody Atividade atividade){
+    public Atividade criarAtividade(@Valid @RequestBody Atividade atividade){
         return atividadeRepository.save(atividade);
     }
+    
+    @GetMapping("buscar-atividade/{id}")
+    public Optional<Atividade> buscarAtividade(@PathVariable Long id) {
+    	Optional<Atividade> atividadeEncontrada = atividadeRepository.findById(id);
+    	return atividadeEncontrada;
+    }
+    
 }
--- src/main/java/service/AtividadeService.java
+++ src/main/java/service/AtividadeService.java
@@ -2,30 +2,31 @@ package service;
 
 
 import model.Atividade;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import repository.AtividadeRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AtividadeService {
-
 
     @Autowired
     private AtividadeRepository atividadeRepository;
-
-    public List<Atividade> findAll() {
-        return  atividadeRepository.findAll();
+    
+    public Atividade criarAtividade(Atividade atividade) {
+        return atividadeRepository.save(atividade);
     }
-
-    public Optional<Atividade> getEvento(Long id) {
+    
+    public List<Atividade> findAll() {
+        return atividadeRepository.findAll();
+    }
+    
+    public Optional<Atividade> getAtividade(Long id) {
         return atividadeRepository.findById(id);
     }
-
-    public Atividade findByIdEvento(Long id) {
-        return atividadeRepository.findByIdEvento(id);
-    }
-
-
 
 }
